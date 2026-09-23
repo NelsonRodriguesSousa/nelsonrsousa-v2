63766e62962e5db0ef4448f748b01e36f7262e13
nelsonrsousa <nelson_85_sousa@hotmail.com>
2026-09-23 12:53:09 +0100

    alteraçoes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { GravelBirdsComponent } from './pages/projects/gravel-birds/gravel-birds.component';
+import { AboutComponent } from './pages/about/about.component';
+import { SoajoOutdoorFestComponent } from './pages/projects/soajo-outdoor-fest/soajo-outdoor-fest.component';
+import { CarrascalComponent } from './pages/projects/carrascal/carrascal.component';
+import { AutalantisComponent } from './pages/projects/autalantis/autalantis.component';
+import { HswComponent } from './pages/projects/hsw/hsw.component';
+import { CafeCentralComponent } from './pages/projects/cafe-central/cafe-central.component';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'gravel-birds', component: GravelBirdsComponent },
+  { path: 'soajo-outdoor-fest', component: SoajoOutdoorFestComponent },
+  { path: 'heading-southwest', component: HswComponent },
+  { path: 'cafe-central', component: CafeCentralComponent },
+  { path: 'refugio-carrascal', component: CarrascalComponent },
+  { path: 'autalantis', component: AutalantisComponent },
+  { path: 'about', component: AboutComponent },
+  { path:'',redirectTo:'/', pathMatch: 'full' }
+]
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './components/menu/menu.component';
+import { HomeComponent } from './pages/home/home.component';
+import { GravelBirdsComponent } from './pages/projects/gravel-birds/gravel-birds.component';
+import { AboutComponent } from './pages/about/about.component';
+import { SoajoOutdoorFestComponent } from './pages/projects/soajo-outdoor-fest/soajo-outdoor-fest.component';
+import { CarrascalComponent } from './pages/projects/carrascal/carrascal.component';
+import { AutalantisComponent } from './pages/projects/autalantis/autalantis.component';
+import { HswComponent } from './pages/projects/hsw/hsw.component';
+import { CafeCentralComponent } from './pages/projects/cafe-central/cafe-central.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    HomeComponent,
+    GravelBirdsComponent,
+    AboutComponent,
+    SoajoOutdoorFestComponent,
+    CarrascalComponent,
+    AutalantisComponent,
+    HswComponent,
+    CafeCentralComponent
   ],
   imports: [
     BrowserModule,
